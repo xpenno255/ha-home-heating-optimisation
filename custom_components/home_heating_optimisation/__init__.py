8d09596ba30d46057852960c50ee9d67467c44af
Spencer Webb <spenno_@yahoo.com>
2026-09-15 13:30:27 +0100

    Satisfy Home Assistant manifest and config schema validation

--- custom_components/home_heating_optimisation/__init__.py
+++ custom_components/home_heating_optimisation/__init__.py
@@ -3,12 +3,15 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import entity_registry as er
 
 from .analytics.coordinator import AnalyticsCoordinator
+from .const import DOMAIN
 from .coordinator import HeatingCoordinator
 from .services import async_register_services
 
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR]
 type HeatingEntry = ConfigEntry[HeatingCoordinator]
 
